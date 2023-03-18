ecc5fa75e186d14342b1b8f254bb353a33d50f31
Tapochela <113248337+Tapochela@users.noreply.github.com>
2023-03-18 23:35:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Security.Policy;
+using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo...");
+            Thread.Sleep(4000);
+            Console.Clear();
+
+            Thread.Sleep(2000);
+            Console.WriteLine("The Matrix has you...");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Follow the white rabbit.");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Knock, knock, Neo.");
+            Thread.Sleep(4000);
+            Console.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
